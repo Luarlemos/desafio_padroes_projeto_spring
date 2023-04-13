67457716a4c13d28cffb4a0d389bee646ed8f9e8
Luarlemos <luarlemos96@hotmail.com>
2023-04-12 22:54:50 -0300

    Desenvolvimento de uma API Rest usando padrões de projeto junto com API ViaCep

---
+++ src/test/java/com/dio/spring/DesafioPadroesProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPadroesProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
